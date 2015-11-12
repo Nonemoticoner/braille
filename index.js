0574b6a1ac2b7782035d30deb6a8722fa5ea20aa
Nonemoticoner <nonemoticoner@gmail.com>
2015-11-12 17:41:46 +0100

    toText fuction added
    New feature!

--- index.js
+++ index.js
@@ -27,6 +27,23 @@ module.exports = {
 		return sign;
 	},
 
+	// converts Braille symbol to letter
+	read: function (symbol) {
+		var letter = undefined;
+
+		// searches sets
+		for (var i = this.sets.ascii.length - 1; i >= 0; i--) {
+			if(this.sets.dot6[i] == symbol){
+				letter = this.sets.ascii[i];
+			}
+		}
+		
+		// if not found will return undefined
+		return letter;
+	},
+
+	// ---------------------------------------
+
 	// converts text to ASCII Braille alphabet
 	toBraille: function (text) {
 		// make given text uppercase
@@ -35,6 +52,26 @@ module.exports = {
 
 		for(var i = 0; i < upper.length; i++) {
 			var symbol = this.convert(upper[i]);
+
+			// unsupported letters will be ommited
+			if(typeof symbol == "undefined"){
+				result += "";
+			}
+			else{
+				result += symbol;
+			}
+		}
+
+		return result;
+	},
+
+	// converts ASCII Braille alphabet to text
+	toText: function (code) {
+		
+		var result = "";
+
+		for(var i = 0; i < code.length; i++) {
+			var symbol = this.read(code[i]);
 
 			// unsupported letters will be ommited
 			if(typeof symbol == "undefined"){
